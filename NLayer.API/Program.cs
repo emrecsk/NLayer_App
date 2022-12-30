75d26b78eb79951ecb41e056e2e6c781feaba912
Emre <emre.coskun@bs.nttdata.com>
2022-12-30 09:52:59 +0100

    DataProtection Added.

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -37,6 +37,7 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddDataProtection();
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
